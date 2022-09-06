42b518b6934f5fa3457552fa1fe0e7f847c686c8
MinionsW <2998394252@qq.com>
2022-09-06 10:03:11 +0800

    nav

---
+++ src/views/NavDesign.vue
@@ -0,0 +1,328 @@
+<template>
+  <div class="nav-design">
+    <van-nav-bar title="免费设计" left-arrow />
+    <img src="/head.png" alt="" />
+    <div>
+      <div class="white">
+        <div class="nav-design-item-img">
+          <div class="bt">
+            <span class="xing">*</span><span>您的房子是?</span>
+          </div>
+          <div v-for="(i, index) in house" :key="index">
+            <img @click="now = index" :src="i" alt="" />
+            <img :src="now == index ? 'duigou.png' : 'noneIcon.png'" />
+          </div>
+        </div>
+      </div>
+      <div class="design-2">
+        <div class="bt">
+          <span class="xing">*</span><span>您计划和谁一起住?</span>
+        </div>
+        <div>
+          <div class="design-2-1">
+            <img
+              @click="now1 = 1"
+              src="https://ikj-storage-front-prod.oss-cn-beijing.aliyuncs.com/app/html-m/mfsj/live1.png"
+              alt=""
+            />
+            <img :src="now1 == 1 ? '/duigou.png' : ''" alt="" />
+            <div>就我自己</div>
+          </div>
+          <div class="design-2-1">
+            <img
+              @click="now1 = 2"
+              src="https://ikj-storage-front-prod.oss-cn-beijing.aliyuncs.com/app/html-m/mfsj/live2.png"
+              alt=""
+            />
+            <img :src="now1 == 2 ? '/duigou.png' : ''" alt="" />
+            <div>和另一半</div>
+          </div>
+          <div class="design-2-1">
+            <img
+              @click="now1 = 3"
+              src="https://ikj-storage-front-prod.oss-cn-beijing.aliyuncs.com/app/html-m/mfsj/live3.png"
+              alt=""
+            />
+            <img :src="now1 == 3 ? '/duigou.png' : ''" alt="" />
+            <div>亲子共住</div>
+          </div>
+        </div>
+        <div class="design-2-2">
+          <div @click="lr = !lr">
+            <img :src="lr ? '/sel2.png' : '/sel1.png'" alt="" />
+            <span>有老人</span>
+          </div>
+          <div @click="cw = !cw">
+            <img :src="cw ? '/sel2.png' : '/sel1.png'" alt="" />
+            <span>有宠物</span>
+          </div>
+        </div>
+      </div>
+      <div class="design-3">
+        <div class="bt">
+          <span class="xing">*</span><span>您更喜欢哪种装修风格?</span>
+        </div>
+        <div class="design-3-1">
+          <div v-for="(img, index) in style" :key="index">
+            <img @click="now3 = index" :src="img" alt="" />
+            <img :src="now3 == index ? 'duigou.png' : 'noneIcon.png'" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="design-3">
+        <div class="bt">
+          <span>你想要的个性空间(可多选)</span>
+        </div>
+        <div class="design-3-1">
+          <div v-for="(img, index) in space" :key="index">
+            <img
+              @click="space[index].dj = !space[index].dj"
+              :src="img.img"
+              alt=""
+            />
+            <img :src="space[index].dj ? 'duigou.png' : 'sel2.png'" />
+          </div>
+        </div>
+      </div>
+      <div class="design-4">
+        <div class="bt"><span class="xing">*</span><span>卫生间偏好</span></div>
+        <div>
+          <span
+            @click="preferenceNow = index"
+            :class="{ active: preferenceNow == index }"
+            v-for="(i, index) in preference"
+            :key="index"
+            >{{ i }}</span
+          >
+        </div>
+      </div>
+      <div class="design-5">
+        <div class="bt">
+          <span>其他装修需求(可多选)</span>
+        </div>
+        <div>
+          <span
+            @click="item.xz = !item.xz"
+            :class="{ active: item.xz }"
+            v-for="(item, index) in other"
+            :key="index"
+            >{{ item.item }}</span
+          >
+        </div>
+      </div>
+      <div class="design-6">
+        <div class="bt"><span class="xing">*</span><span>房屋状态</span></div>
+        <div>
+          <div @click="HouseStatu = !HouseStatu">
+            <img :src="HouseStatu ? 'sel1.png' : 'sel2.png'" alt="" />
+            老房
+          </div>
+          <div @click="HouseStatu = !HouseStatu">
+            <img :src="HouseStatu ? 'sel2.png' : 'sel1.png'" alt="" />
+            新房
+          </div>
+        </div>
+        <div><input type="text" /></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      HouseStatu: false,
+      now: null,
+      now1: null,
+      now3: null,
+      lr: true,
+      cw: true,
+      preferenceNow: null,
+      house: ["house1.png", "house2.png", "house3.png", "house4.png"],
+      style: [
+        "style1.png",
+        "style2.png",
+        "style3.png",
+        "style4.png",
+        "style5.png",
+        "style6.png",
+        "style7.png",
+      ],
+      space: [
+        { img: "space1.png", dj: false },
+        { img: "space2.png", dj: false },
+        { img: "space3.png", dj: false },
+        { img: "space4.png", dj: false },
+        { img: "space5.png", dj: false },
+        { img: "space6.png", dj: false },
+      ],
+      preference: ["干湿分离", "浴缸", "淋浴房"],
+      other: [
+        { item: "定制家具", xz: false },
+        { item: "暖气", xz: false },
+        { item: "全屋净水", xz: false },
+        { item: "防盗门", xz: false },
+        { item: "外窗", xz: false },
+        { item: "热水器", xz: false },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.white {
+  background-color: white;
+}
+.bt {
+  margin: 0 15px;
+}
+.xing {
+  color: red;
+}
+.nav-design {
+  background-color: #f5f5f5;
+  > img {
+    width: 100%;
+  }
+  > div:nth-child(3) {
+    margin: 15px;
+    border-radius: 8px;
+  }
+}
+.nav-design-item-img {
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  div {
+    width: 48%;
+    position: relative;
+    margin-top: 10px;
+    > img:nth-child(1) {
+      width: 100%;
+    }
+    > img:nth-child(2) {
+      position: absolute;
+      right: 8px;
+      top: 8px;
+      width: 25px;
+    }
+  }
+}
+.design-2 {
+  margin-top: 20px;
+  padding: 10px 0;
+  background-color: white;
+  > div:nth-child(2) {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .design-2-1 {
+    margin-top: 15px;
+    position: relative;
+    width: 30%;
+    text-align: center;
+
+    > img:nth-child(1) {
+      width: 80%;
+    }
+    > img:nth-child(2) {
+      position: absolute;
+      bottom: 15px;
+      right: 0px;
+      width: 25px;
+    }
+  }
+}
+.design-2-2 {
+  display: flex;
+  margin: 20px 0;
+  > div {
+    display: flex;
+    border: 1px solid #dddddd;
+    padding: 10px;
+    margin-left: 20px;
+    border-radius: 50px;
+    > span {
+      font-size: 25px;
+      margin-right: 10px;
+    }
+    > img {
+      padding-top: 6px;
+      width: 20px;
+      height: 20px;
+      margin: 0 10px;
+    }
+  }
+}
+.design-3 {
+  margin-top: 40px;
+  padding: 10px 0;
+  background-color: white;
+  .design-3-1 {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 10px;
+    > div {
+      width: 32%;
+      position: relative;
+      > img:nth-child(1) {
+        width: 100%;
+      }
+      > img:nth-child(2) {
+        width: 20px;
+        position: absolute;
+        right: 5px;
+        top: 5px;
+      }
+    }
+  }
+}
+.design-4 {
+  margin: 20px 0;
+  background-color: white;
+  padding: 10px 0;
+  > div:nth-child(2) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    > span {
+      margin: 10px;
+      width: 20%;
+      text-align: center;
+      border: 1px solid #f1f2f4;
+      padding: 5px 10px;
+      &.active {
+        border-color: #ffaa6e;
+        color: #ffaa6e;
+      }
+    }
+  }
+}
+.design-5 {
+  margin: 20px 0;
+  background-color: white;
+  padding: 10px 0;
+  > div:nth-child(2) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    > span {
+      border-radius: 20px;
+      margin: 10px;
+      width: 20%;
+      text-align: center;
+      border: 1px solid #f1f2f4;
+      padding: 5px 10px;
+      &.active {
+        border-color: #ffaa6e;
+        color: #ffaa6e;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/NavDetail.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <van-nav-bar title="设计师" left-arrow />
+    <img
+      class="nav-detail"
+      src="https://ikj-storage-front-prod.oss-cn-beijing.aliyuncs.com/app/html-m/mxsjs/banner.png"
+      alt=""
+    />
+    <ul>
+      <span>设计师榜</span>
+      <li v-for="i in 20" :key="i">
+        <div>
+          <div>
+            <div>
+              <img
+                class="img-portrait"
+                src="https://ikj-public.oss-cn-beijing.aliyuncs.com/market/f6ac5cd8/c26e956c/7b635a95/d0524ddf/5pyq5qCH6aKYLTFfMjAyMi0wOC0yNVQxNTo0Mzo1Ny4xMTY=.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>刘</h4>
+              <div>服务案例...</div>
+              <div>设计风格</div>
+            </div>
+          </div>
+          <div>
+            <span>用户质询</span>
+            <van-button size="mini" type="info">1v1和TA沟通</van-button>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://ikj-public.oss-cn-beijing.aliyuncs.com/market/d8a7f1e3/0d0cebe8/a7866103/40f20a4f/5a6i6aSQ5Y6FICgxKV8yMDIyLTA4LTI1VDE1OjU3OjQ5LjkxNw==.jpg"
+            alt=""
+          />
+          <img
+            src="https://ikj-public.oss-cn-beijing.aliyuncs.com/market/d8a7f1e3/0d0cebe8/a7866103/40f20a4f/5pyq5qCH6aKYLTFfMjAyMi0wOC0yNVQxNTo1ODowMC40NTI=.jpg"
+            alt=""
+          />
+          <img
+            src="https://ikj-public.oss-cn-beijing.aliyuncs.com/market/d8a7f1e3/0d0cebe8/a7866103/40f20a4f/5pyq5qCH6aKYLTFfMjAyMi0wOC0yNVQxNTo1ODoxMC43OTM=.jpg"
+            alt=""
+          />
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.nav-detail {
+  width: 100vw;
+}
+ul {
+  margin: 15px;
+  li {
+    padding: 15px;
+    background-color: #fafafa;
+    margin: 10px;
+    border-radius: 5px;
+    > div:nth-child(2) {
+      display: flex;
+      justify-content: space-between;
+      > img {
+        width: 30%;
+        border-radius: 5px;
+      }
+    }
+    > div:nth-child(1) {
+      display: flex;
+      justify-content: space-between;
+      > div:nth-child(2) {
+        display: flex;
+        flex-direction: column;
+        margin-top: 10px;
+      }
+      > div:nth-child(1) {
+        display: flex;
+        > div:nth-child(2) {
+          margin-left: 10px;
+          > h4 {
+            padding: 0;
+            margin: 0;
+          }
+          > div {
+            font-size: 10px;
+            margin-top: 3px;
+          }
+        }
+      }
+    }
+  }
+  .img-portrait {
+    border-radius: 50%;
+    width: 70px;
+  }
+}
+</style>
