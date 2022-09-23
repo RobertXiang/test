1cbf788178afecc7549e346b440ecf4937dd145b
RobertXiang <1308071827@qq.com>
2022-09-23 10:09:55 +0800

    聊天室服务器

--- app.js
+++ app.js
@@ -1,86 +1,23 @@
-//引入第三方模块
-//加载express模块
-const express = require('express') //npm install express
-//加载mysql模块
-const mysql = require('mysql') //npm install  mysql
-//加载cors模块
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-//创建服务器
-const app = express()
 
-//定义端口并进行监听
-const port = 3000
-app.listen(port, (req, res) => {
-  console.log('已成功监听端口:' + port)
-})
+io.on("connection", (socket) => {
+  console.log("有客户端连进来了:" + socket.id);
+  socket.on("textmsg", function (data) {
+    console.log(data);
+    io.emit("textmsg", data);
+  });
+});
 
-//应用中间件 处理post 传参
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-)
-
-//静态托管
-app.use(express.static('./public'))
-
-// 使用CORS中间件,解决跨域问题,添加白名单
-app.use(
-  cors({
-    origin: [
-      'http://localhost:8080',
-      'http://127.0.0.1:8080',
-      'http://localhost:8081',
-      'http://127.0.0.1:8081',
-      'http://localhost:8082',
-      'http://127.0.0.1:8082',
-    ],
-  })
-)
-
-//引入用户登录注册的路由器接口
-const userRouter = require('./routers/user.js')
-app.use('/user', userRouter)
-
-// 引入设计师及详情路由器接口
-const designerRouter = require('./routers/designer.js')
-app.use('/designer', designerRouter)
-
-// 设计师案例详情
-const des_deilesRouter = require('./routers/des_deiles.js')
-app.use('/des_deiles', des_deilesRouter)
-
-//案例详情路由
-const anliRouter = require('./routers/anli.js')
-app.use('/anli', anliRouter)
-
-//商场商品路由
-const shoppingRouter = require('./routers/shopping.js')
-app.use('/shop', shoppingRouter)
-
-// 收货地址路由
-const addressRouter = require('./routers/address.js')
-app.use('/address', addressRouter)
-
-// 用户订单路由
-const orderRouter = require('./routers/order.js')
-app.use('/order', orderRouter)
-
-// 购物车路由
-const cartRouter = require('./routers/cart.js')
-app.use('/cart', cartRouter)
-
-// 免费设计页面路由
-const mfshejiRouter = require('./routers/mfsheji.js')
-app.use('/mfsj', mfshejiRouter)
-
-// 商品订单路由
-const pro_orderRouter = require('./routers/productOrder.js')
-app.use('/pro_order', pro_orderRouter)
-
-//错误处理
-app.use((err, req, res, next) => {
-  console.log(err)
-  res.send({ code: 505, msg: '服务器端错误' })
-})
+server.listen(3030, () => {
+  console.log("websocket服务已启动，正在监听 *:3030");
+});
