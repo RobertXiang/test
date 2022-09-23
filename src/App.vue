62413681b2439113046b8d4da361167e55b12c64
RobertXiang <1308071827@qq.com>
2022-09-23 13:00:53 +0800

    后台客服消息样式修改

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,50 @@
   <div id="app">
     <router-view />
     <ke-fu v-if="$store.state.close" :msg="khMsg"></ke-fu>
-    <div class="kf-msg" v-if="show">消息+{{khMsg.length}}</div>
+    <div class="kf-msg" v-if="show">消息+{{ khMsg.length }}</div>
   </div>
 </template>
 <script scr="../public/js/socket.io.js"></script>
 
 <script>
-import KeFu from "./components/KeFu.vue";
-import io from "../public/js/socket.io.js"
-var socket= io("http://localhost:3030/")
+import KeFu from './components/KeFu.vue'
+import io from '../public/js/socket.io.js'
+var socket = io('http://localhost:3030/')
 export default {
   components: { KeFu },
   data() {
     return {
-    
       khMsg: [],
       show: false,
-    };
+    }
   },
   computed: {
     liuyan() {
-      return this.$store.state.close;
+      return this.$store.state.close
     },
   },
   mounted() {
-    socket.on("textmsg", (data) => {
-      this.show = true;
+    socket.on('textmsg', data => {
+      this.show = true
       if (data.name != this.$store.state.uname) {
-        this.khMsg.push(data);
+        this.khMsg.push(data)
       }
-    });
+    })
   },
   watch: {
     liuyan() {
       if (this.liuyan == true) {
-        console.log("对话打开,显示消息");
-        socket.close();
+        console.log('对话打开,显示消息')
+        socket.close()
       } else {
-        this.show = false;
-        console.log("对话关闭:监听消息");
-        this.khMsg=[]
-        socket.open();
+        this.show = false
+        console.log('对话关闭:监听消息')
+        this.khMsg = []
+        socket.open()
       }
     },
   },
-};
+}
 </script>
 
 <style lang="scss">
@@ -55,7 +54,7 @@ export default {
   height: 20px;
   font-size: 12px;
   position: fixed;
-  top: 513px;
+  top: 405px;
   left: 173px;
   background-color: orange;
   border-radius: 4px;
